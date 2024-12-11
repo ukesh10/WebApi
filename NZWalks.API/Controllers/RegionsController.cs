a66e326485f3e0c448a5f3b9f76224af498f8df7
Ukesh <ukeshsharma10@gmail.com>
2024-12-11 21:53:11 +0530

    Added RegionsController including features like async/await, automapper, DTOs, Repository Patter

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var regionsDomain = await regionRepository.GetAllAsync();
+            //Map Domain Model to DTO
+            //var regionsDto = new List<RegionDTO>();
+            //foreach (var regionDomain in regionsDomain)
+            //{
+            //    regionsDto.Add(new RegionDTO()
+            //    {
+            //        Id = regionDomain.Id,
+            //        Name = regionDomain.Name,
+            //        Code = regionDomain.Code,
+            //        RegionImageUrl = regionDomain.RegionImageUrl,
+            //    });
+            //}
+            var regionsDto = mapper.Map<List<RegionDTO>>(regionsDomain);
+            //return DTO
+            return Ok(regionsDto);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetById([FromRoute] Guid id)
+        {
+            var regionDomain = await regionRepository.GetByIdAsync(id);
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+            //Map Domain Model to DTO
+            var regionDto = mapper.Map<RegionDTO>(regionDomain);
+            //return Dto back to client
+            return Ok(regionDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] AddRegionRequestDTO addRegionRequestDTO)
+        {
+            // Map DTO to Domain Model
+            var regionDomain = mapper.Map<Region>(addRegionRequestDTO);
+            // Use Domain model to create Region
+            regionDomain = await regionRepository.CreateAsync(regionDomain);
+
+            //Map Domain Model back to Dto
+            var regionDto = mapper.Map<RegionDTO>(regionDomain);
+            return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDTO updateRegionRequestDTO)
+        {
+            // Map DTO to Domain Model
+            var regionDomain = mapper.Map<Region>(updateRegionRequestDTO);
+            regionDomain = await regionRepository.UpdateAsync(id, regionDomain);
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+            //Convert Domain Model to DTO
+            var regionDto = mapper.Map<RegionDTO>(regionDomain);
+            return Ok(regionDto);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var regionDomain = await regionRepository.DeleteAsync(id);
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+            // map Domain model to DTO
+            var regionDto = mapper.Map<RegionDTO>(regionDomain);
+            return Ok(regionDto);
+        }
+    }
+}
--- NZWalks.API/Program.cs
+++ NZWalks.API/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
+using NZWalks.API.Mappings;
+using NZWalks.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,8 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<NZWalksDbContext>(options => 
 options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalksConnectionString")));
+
+builder.Services.AddScoped<IRegionRepository, SQLRegionRepository>();
+
+builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
